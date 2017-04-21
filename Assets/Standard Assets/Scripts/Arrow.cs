841d7367aa06a5a126962588b352dea7bfd36fda
sony <sony9a8@yahoo.com>
2017-04-20 21:15:18 -0700

    Arrow Stick with blood splatter

--- Assets/Standard Assets/Scripts/Arrow.cs
+++ Assets/Standard Assets/Scripts/Arrow.cs
@@ -6,6 +6,8 @@ public class Arrow : MonoBehaviour {
 	public GameObject explosionObject;
 	public float mSpeed = 100f;
 
+	public bool mInFlight;
+
 	// Private
 	private RaycastHit mHit;
 
@@ -15,29 +17,33 @@ public class Arrow : MonoBehaviour {
 			if (explosionObject && col.tag == "Monster") {
 				GameObject newExplosion = (GameObject)Instantiate (explosionObject, transform.position, transform.rotation);
 				Debug.Log ("sonydb: blood explosion");
+				transform.parent = col.transform;
 				Destroy (newExplosion, 2f);
 			}
-			Destroy (this.gameObject);
+			mInFlight = false;
+			Destroy (this.gameObject, 4f);
 		}
 	}
 
 	// Use this for initialization
 	void Start () {
-	
+		mInFlight = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 nextPosition = transform.position + (transform.forward * mSpeed + Vector3.down * 10f * Time.deltaTime) * Time.deltaTime;
+		if (mInFlight) {
+			Vector3 nextPosition = transform.position + (transform.forward * mSpeed + Vector3.down * 10f * Time.deltaTime) * Time.deltaTime;
 
-		if (Physics.Raycast (transform.position, transform.forward, out mHit, 5f)) {
-			if ((mHit.collider.tag == "Terrain" || mHit.collider.tag == "Monster") &&
-				mHit.distance < (nextPosition - transform.position).magnitude) {
-				Debug.Log ("sonydb: raycast at work");
-				nextPosition = mHit.point;
+			if (Physics.Raycast (transform.position, transform.forward, out mHit, 5f)) {
+				if ((mHit.collider.tag == "Terrain" || mHit.collider.tag == "Monster") &&
+				    mHit.distance < (nextPosition - transform.position).magnitude) {
+					Debug.Log ("sonydb: raycast at work");
+					nextPosition = mHit.point;
+				}
 			}
+			transform.forward = (nextPosition - transform.position).normalized;
+			transform.position = nextPosition;
 		}
-		transform.forward = (nextPosition - transform.position).normalized;
-		transform.position = nextPosition;
 	}
 }
